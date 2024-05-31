6d49fc2ee0741b494465bce23a0baf0d1d00ea05
Amaro Elias <amaronascimento2016@hotmail.com>
2024-05-30 21:06:38 -0300

    ajustes de margem nivel 1

--- src/app/components/levels/one/screen-five-level-one/screen-five-level-one.component.ts
+++ src/app/components/levels/one/screen-five-level-one/screen-five-level-one.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./screen-five-level-one.component.css']
 })
 export class ScreenFiveLevelOneComponent implements OnInit {
+  
+  rectangleStatus: boolean[] = [false, false, false, false, false];
+  numbers: number[] = [1, 1, 1, 1, 1];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleRectangle(index: number): void {
+    this.rectangleStatus[index - 1] = !this.rectangleStatus[index - 1];
+    if (this.rectangleStatus[index - 1]) {
+      this.numbers[index - 1] = 0;
+    } else {
+      this.numbers[index - 1] = 1;
+    }
+  }
 }
