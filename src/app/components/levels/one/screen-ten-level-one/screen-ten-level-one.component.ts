6f5509ef6b59971e4a820952bb351fb73b38d9d8
amaroelias <amaronascimento2016@hotmail.com>
2024-12-16 00:47:43 -0300

    fase 1 com feedback

--- src/app/components/levels/one/screen-ten-level-one/screen-ten-level-one.component.ts
+++ src/app/components/levels/one/screen-ten-level-one/screen-ten-level-one.component.ts
@@ -1,7 +1,6 @@
-import { ToastService } from '../../toast.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ViewChild} from '@angular/core';
 import { Router } from '@angular/router';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { NotificationComponent } from '../../../notification/notification.component';
 
 @Component({
   selector: 'app-screen-ten-level-one',
@@ -10,77 +9,56 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 })
 export class ScreenTenLevelOneComponent implements OnInit {
 
-  rectangles: boolean[] = [true, false, true, false, true];
+  @ViewChild(NotificationComponent) notification!: NotificationComponent;
 
-  flip1: string = 'inactive';
-  flip2: string = 'active';
-  flip4: string = 'active';
-  flip8: string = 'inactive';
-  flip16: string = 'active';
-
-  byn1: number = 1;
-  byn2: number = 0;
-  byn4: number = 0;
-  byn8: number = 1;
-  byn16: number = 0;
-
-  btnClass1: string = "";
-  btnClass2: string = "";
-  btnClass3: string = "";
-  btnClass4: string = "";
-
-  attempts: number = 0;
-
-  question: string = "Como você acha que eram codificadas as mensagens enviadas através de uma máquina de fax?";
-
+  // alternativas
   answers: string[] = ["Como representação que economize a quantidade de dados enviada","Usando pontos pretos como 0 e brancos como 1"];
 
-  constructor(private router: Router, public toastService: ToastService) {
 
+  buttonClasses: { [key: number]: string } = {
+    1: "",
+    2: "",
+    3: "",
+    4: "",
+  };
+
+  constructor(private router: Router) {
   }
 
   ngOnInit(): void {
     this.answers.sort(() => Math.random() - 0.5);
   }
 
+  // validar resposta
   changeAnswers(value: string, btn: number): void {
-    if(value === "Como representação que economize a quantidade de dados enviada") {
-        this.buttonClass(btn, true);
-        this.toastService.show('Você acertou!');
-        setTimeout(() => {
-          this.router.navigate(['fase-1-11']);
-        },1000);
+    if (value === "Como representação que economize a quantidade de dados enviada") {
+      this.buttonClass(btn, true);
+      this.onSuccess();
+      setTimeout(() => {
+        this.router.navigate(['fase-1-11']);
+      }, 1000);
     } else {
-        this.buttonClass(btn, false);
+      this.buttonClass(btn, false);
+      this.onError();
     }
   }
 
-  pickAnswer(answer: string): void {
-    if(answer !== "Como representação que economize a quantidade de dados enviada") {
-      this.toastService.show('Tente outra vez.');
-      this.attempts += 1;
-      console.log(this.attempts);
-    }
+  // resposta certa
+  onSuccess(): void {
+    this.notification.show('Você acertou!', 'success');
   }
 
+  // resposta errada
+  onError(): void {
+    this.notification.show('Tente outra vez.', 'error');
+  }
+
+  // botões alternativas
   buttonClass(button: number, status: boolean): void {
-    if(button == 1) {
-      this.btnClass1 = status ? "correct" : "incorrect";
-      setTimeout(() => {this.btnClass1 = "";},1000);
-    }
-    if(button == 2) {
-      this.btnClass2 = status ? "correct" : "incorrect";
-      setTimeout(() => {this.btnClass2 = "";},1000);
-    }
-    if(button == 3) {
-      this.btnClass3 = status ? "correct" : "incorrect";
-      setTimeout(() => {this.btnClass3 = "";},1000);
-    }
-    if(button == 4) {
-      this.btnClass4 = status ? "correct" : "incorrect";
-      setTimeout(() => {this.btnClass4 = "";},1000);
-    }
-
+    this.buttonClasses[button] = status ? "correct" : "incorrect";
+    setTimeout(() => {
+      this.buttonClasses[button] = "";
+    }, 1000);
   }
-
+  
 }
