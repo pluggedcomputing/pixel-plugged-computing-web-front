4c0ce6dd6e3ff933075efcd0d3f3323c9ba982ab
amaroelias <amaronascimento2016@hotmail.com>
2025-06-08 01:12:44 -0300

    feat: level 3 internacionalizado

--- src/app/components/levels/three/screen-eleven-level-three/screen-eleven-level-three.component.ts
+++ src/app/components/levels/three/screen-eleven-level-three/screen-eleven-level-three.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MessagesService } from 'src/app/service/messages/messages.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-screen-eleven-level-three',
@@ -13,47 +15,55 @@ export class ScreenElevenLevelThreeComponent implements OnInit {
   estrelasPreenchidas: number = 0;
   estrelasVazias: number = 0;
   mensagemFinal: string = '';
+  mensagens: string[] = [];
 
-  constructor() { }
+  constructor(private messagesService: MessagesService,
+    private translate: TranslateService
+  ) { }
 
   ngOnInit(): void {
     this.resultados = JSON.parse(localStorage.getItem('resultados') || '[]');
     this.acertos = this.resultados.filter(resultado => resultado === 'acerto').length;
     this.erros = this.resultados.filter(resultado => resultado === 'erro').length;
     this.calcularEstrelas();
+    this.translate.get('congratulationsScreen.motivationalMessages').subscribe((text) => {
+      this.mensagens = text;
+    });
     this.definirMensagemFinal();
   }
 
-   // Array de estrelas
-   getArray(length: number): any[] {
+  // Array de estrelas
+  getArray(length: number): any[] {
     return new Array(length);
   }
 
-    // Método para calcular as estrelas
-    calcularEstrelas(): void {
-      const totalEstrelas = 5; // Total de estrelas a serem exibidas
-  
-      // Calcula a proporção de acertos e erros
-      const proporcaoAcertos = this.acertos / (this.acertos + this.erros);
-  
-      // Calcula a quantidade de estrelas amarelas e brancas
-      this.estrelasPreenchidas = Math.round(totalEstrelas * proporcaoAcertos);
-      this.estrelasVazias = totalEstrelas - this.estrelasPreenchidas;
+  // Método para calcular as estrelas
+  calcularEstrelas(): void {
+    const totalEstrelas = 5; // Total de estrelas a serem exibidas
+
+    // Calcula a proporção de acertos e erros
+    const proporcaoAcertos = this.acertos / (this.acertos + this.erros);
+
+    // Calcula a quantidade de estrelas amarelas e brancas
+    this.estrelasPreenchidas = Math.round(totalEstrelas * proporcaoAcertos);
+    this.estrelasVazias = totalEstrelas - this.estrelasPreenchidas;
+  }
+
+  definirMensagemFinal(): void {
+
+    switch (this.estrelasPreenchidas) {
+      case 5: this.mensagemFinal = this.mensagens[0];
+        break;
+      case 4: this.mensagemFinal = this.mensagens[1];
+        break;
+      case 3: this.mensagemFinal = this.mensagens[2];
+        break;
+      case 2: this.mensagemFinal = this.mensagens[3];
+        break;
+      default: this.mensagemFinal = this.mensagens[4];
     }
-  
-    definirMensagemFinal(): void {
-      if (this.estrelasPreenchidas === 5) {
-        this.mensagemFinal = 'Incrível! Você acertou tudo!';
-      } else if (this.estrelasPreenchidas === 4) {
-        this.mensagemFinal = 'Muito bom! Quase perfeito!';
-      } else if (this.estrelasPreenchidas === 3) {
-        this.mensagemFinal = 'Bom trabalho! Continue praticando!';
-      } else if (this.estrelasPreenchidas === 2) {
-        this.mensagemFinal = 'Você está no caminho certo! Vamos melhorar!';
-      } else {
-        this.mensagemFinal = 'Tudo é aprendizagem! Tente novamente!';
-      }
-    }
+
+  }
 
   ngOnDestroy(): void {
     localStorage.removeItem('resultados'); // Remove os resultados do localStorage
