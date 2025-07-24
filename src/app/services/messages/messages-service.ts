45564238019ca30b8ad6b41f111400d245279864
amaroelias <amaronascimento2016@hotmail.com>
2025-07-23 22:21:35 -0300

    feat: adicionado service para salvar idioma no localStorage

---
+++ src/app/services/messages/messages-service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessagesService {
+  
+    constructor(private translate: TranslateService) {
+      const languageSave = localStorage.getItem('language') || 'pt-br';
+      translate.setDefaultLang('pt-br');
+      translate.use(languageSave);
+    }
+  
+    setLanguage(language: string) {
+      this.translate.use(language);
+      localStorage.setItem('language', language);
+    }
+  
+}
