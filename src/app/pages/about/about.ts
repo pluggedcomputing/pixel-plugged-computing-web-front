b94d661973a0aa56c4328d663557de668477f752
amaroelias <amaronascimento2016@hotmail.com>
2025-07-23 22:27:22 -0300

    feat: tela about adicionada

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { ExitButton } from '../../components/buttons/exit-button/exit-button';
+import { MessagesService } from '../../services/messages/messages-service';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  standalone: true,
+  imports: [TranslateModule, ExitButton],
   templateUrl: './about.html',
   styleUrl: './about.scss'
 })
 export class About {
-
+  constructor(private messages: MessagesService){
+  }
 }
