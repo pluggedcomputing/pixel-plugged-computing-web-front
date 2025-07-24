de42e5b07172d305cb9c5fab9d7caec0a855eccd
amaroelias <amaronascimento2016@hotmail.com>
2025-07-23 22:24:11 -0300

    style: responsividade da tela home melhorada

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { LanguageSelection } from '../../components/language-selection/language-selection';
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LanguageSelection],
+  imports: [LanguageSelection, TranslateModule, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
+
 }
