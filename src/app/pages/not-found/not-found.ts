c0ca294f9b3173643ef76ee31cdeb3293cfbfbcd
amaroelias <amaronascimento2016@hotmail.com>
2025-07-29 18:48:02 -0300

    feat: page not found adicionada

--- src/app/pages/not-found/not-found.ts
+++ src/app/pages/not-found/not-found.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { MessagesService } from '../../services/messages/messages-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  standalone: true,
+  imports: [TranslateModule],
   templateUrl: './not-found.html',
-  styleUrl: './not-found.scss'
+  styleUrl: './not-found.scss',
 })
 export class NotFound {
+  constructor(
+    private messagesService: MessagesService,
+    private router: Router
+  ) {}
 
+  navigateTo() {
+    this.router.navigate(['/home']);
+  }
 }
