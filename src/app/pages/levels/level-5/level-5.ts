5b5364d6c73ed00f0cd75ad1aa7e723418a6d3e2
amaroelias <amaronascimento2016@hotmail.com>
2025-08-08 10:11:28 -0300

    feat: telas iniciais level 5

--- src/app/pages/levels/level-5/level-5.ts
+++ src/app/pages/levels/level-5/level-5.ts
@@ -1,20 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { ExitButton } from '../../../components/buttons/exit-button/exit-button';
 import { QrCode } from '../../../components/qr-code/qr-code';
+import { CardHost } from '../../../components/card-host/card-host';
 
 @Component({
   selector: 'app-level-5',
   standalone: true,
-  imports: [ExitButton, QrCode],
+  imports: [ExitButton, QrCode, CardHost],
   templateUrl: './level-5.html',
   styleUrl: './level-5.scss',
 })
 export class Level5 {
   level: string = '5';
-  text = signal('Hello');
 
-  updateText(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    this.text.set(input.value);
-  }
+  // Qr Code
+
+  // text = signal('');
+  // updateText(event: Event): void {
+  //   const input = event.target as HTMLInputElement;
+  //   this.text.set(input.value);
+  // }
 }
