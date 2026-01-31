e9981eeb54763dd7ec280294ff6fa8a5d76098e9
amaroelias <amaronascimento2016@hotmail.com>
2026-01-31 00:48:54 -0300

    test: fluxo esperado da tela about feito

--- cypress/e2e/about/about.cy.ts
+++ cypress/e2e/about/about.cy.ts
@@ -1,10 +1,20 @@
 describe('About', () => {
-  it('Elementos principais', () => {
+  beforeEach(() => {
     cy.visit('/about');
+  });
 
+  it('Elementos principais', () => {
     cy.getByData('about-title').should('be.visible');
     cy.getByData('about-paragraph1').should('be.visible');
     cy.getByData('about-paragraph2').should('be.visible');
     cy.getByData('about-paragraph3').should('be.visible');
+  });
+
+  it('Redirecionar para site', () => {
+    cy.getByData('about-img-cp').click();
+    cy.url().should(
+      'eq',
+      'https://sites.google.com/view/computacaoplugada/avalia%C3%A7%C3%B5es-apps',
+    );
   });
 });
